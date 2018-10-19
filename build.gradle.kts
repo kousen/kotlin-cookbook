28c1ca5ae50dda9daf2dc11f2a7e0471aac56da4
Ken Kousen <ken.kousen@kousenit.com>
2018-10-19 13:52:59 -0700

    Add Gradle build using Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `java-library`
+    kotlin("jvm") version "1.3.0-rc-190"
+}
+
+group = "com.kousenit"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap") }
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.3.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
