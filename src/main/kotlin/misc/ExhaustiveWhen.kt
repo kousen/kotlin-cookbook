4659b2af79067049d1145e6d73c74a8943bcfac0
Ken Kousen <ken.kousen@kousenit.com>
2018-11-25 17:59:38 -0500

    Add demo of exhaustive for when

---
+++ src/main/kotlin/misc/ExhaustiveWhen.kt
@@ -0,0 +1,36 @@
+package misc
+
+val <T> T.exhaustive: T
+    get() = this
+
+fun printMod3(n: Int) {
+    when (n % 3) {
+        0 -> println("$n % 3 == 0")
+        1 -> println("$n % 3 == 1")
+        2 -> println("$n % 3 == 2")
+    }
+}
+
+fun printMod3SingleStatement(n: Int) =
+    when (n % 3) {
+        0 -> println("$n % 3 == 0")
+        1 -> println("$n % 3 == 1")
+        2 -> println("$n % 3 == 2")
+        else -> println("Houston, we have a problem...")
+    }
+
+fun printMod3Exhaustive(n: Int) {
+    when (n % 3) {
+        0 -> println("$n % 3 == 0")
+        1 -> println("$n % 3 == 1")
+        2 -> println("$n % 3 == 2")
+        else -> println("Houston, we have a problem...")
+    }.exhaustive
+}
+
+
+fun main(args: Array<String>) {
+    (1..10).forEach { printMod3(it) }
+    (1..10).forEach { printMod3SingleStatement(it) }
+    (1..10).forEach { printMod3Exhaustive(it) }
+}
--- src/main/kotlin/misc/inheritance.kt
+++ src/main/kotlin/misc/inheritance.kt
@@ -1,4 +1,4 @@
-package collections
+package misc
 
 import java.text.NumberFormat
 
