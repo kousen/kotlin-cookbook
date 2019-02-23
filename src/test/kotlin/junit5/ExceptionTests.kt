c908746b5ae2e2c47191fe1302839fc07c441840
Ken Kousen <ken.kousen@kousenit.com>
2019-02-23 16:07:41 -0500

    Suppress warnings in intentionally-thrown exception demo

--- src/test/kotlin/junit5/ExceptionTests.kt
+++ src/test/kotlin/junit5/ExceptionTests.kt
@@ -5,15 +5,14 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class ExceptionTests {
+    @Suppress("DIVISION_BY_ZERO")
     @Test
     fun `dividing by zero is bad`() {
         val x = 1
         val y = 0
         val exception = assertThrows<ArithmeticException> {
-            val z = x / y
+            x / y
         }
         assertEquals("/ by zero", exception.message)
     }
-
-
 }
