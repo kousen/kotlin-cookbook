f2e0866e28b3d52be111d8c7aef96426c0700dfc
Ken Kousen <ken.kousen@kousenit.com>
2019-03-01 13:51:43 -0500

    Add example of measureTimeMillis

---
+++ src/main/kotlin/misc/MeasureTime.kt
@@ -0,0 +1,31 @@
+package misc
+
+import java.util.stream.IntStream
+import kotlin.system.measureNanoTime
+import kotlin.system.measureTimeMillis
+
+fun doubleIt(x: Int): Int {
+    Thread.sleep(100L)
+    println("doubling $x with on thread ${Thread.currentThread().name}")
+    return x * 2
+}
+
+fun main() {
+    println("This machine has ${Runtime.getRuntime().availableProcessors()} processors")
+
+    var time = measureTimeMillis {
+        IntStream.rangeClosed(1, 6)
+            .map { doubleIt(it) }
+            .sum()
+    }
+    println("Sequential stream took ${time}ms")
+
+    time = measureTimeMillis {
+        IntStream.rangeClosed(1, 6)
+            .parallel()
+            .map { doubleIt(it) }
+            .sum()
+    }
+    println("Parallel stream took ${time}ms")
+
+}
