78ab33dcee064e185dd4139b2e89bc2609cd0fff
Ken Kousen <ken.kousen@kousenit.com>
2019-10-03 01:07:15 -0400

    Add array tests

---
+++ src/test/kotlin/collections/ArrayTests.kt
@@ -0,0 +1,45 @@
+package collections
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.*
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+public class ArrayTests {
+    @Test
+    internal fun `create with arrayOf`() {
+        val strings = arrayOf("this", "is", "an", "array", "of", "strings")
+        assertTrue(strings is Array<String>)
+    }
+
+    @Test
+    internal fun `array of nulls`() {
+        val nullStringArray = arrayOfNulls<String>(5)
+        for (x in nullStringArray) {
+            assertNull(x)
+        }
+    }
+
+    @Test
+    internal fun `valid indices`() {
+        val strings = arrayOf("this", "is", "an", "array", "of", "strings")
+        val indices = strings.indices
+        assertThat(indices, contains(0, 1, 2, 3, 4, 5))
+    }
+
+    @Test
+    internal fun `withIndex returns IndexValues`() {
+        val strings = arrayOf("this", "is", "an", "array", "of", "strings")
+        for ((index, value) in strings.withIndex()) {
+            println("Index $index maps to $value")
+            assertTrue(index in 0..5)
+        }
+    }
+
+    @Test
+    internal fun `constructor with function`() {
+        val squares = Array(5) { i -> (i * i).toString() }
+        assertThat(squares, `is`(arrayContaining("0", "1", "4", "9", "16")))
+    }
+}
---
+++ src/test/kotlin/oop/PersonTest.kt
@@ -0,0 +1,51 @@
+package oop
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+data class Person(
+    val first: String,
+    val middle: String?,
+    val last: String
+)
+
+class PersonTest {
+    private val jkRowling = Person("Joanne", null, "Rowling")
+
+    @Test
+    fun `nullable middle name`() {
+        assertNull(jkRowling.middle)
+    }
+
+    @Test
+    fun `safe call operator`() {
+
+        val p = Person(first = "North", middle = null, last = "West")
+        var middleNameLength = p.middle?.length
+        assertTrue(middleNameLength is Int?)
+
+        if (p.middle != null) {
+            middleNameLength = p.middle.length    // smart cast
+        }
+    }
+
+    @Test
+    fun `not-null assertion operator`() {
+        var p2 = Person(first = "North", middle = null, last = "West")
+        if (p2.middle != null) {
+            val middleNameLength = p2.middle!!.length  // ugh
+        }
+    }
+
+    @Test
+    internal fun `Elvis operator`() {
+        val p = Person(first = "North", middle = null, last = "West")
+        val middleNameLength = p.middle?.length ?: 0
+    }
+
+    @Test
+    internal fun `safe cast`() {
+        val person = jkRowling as? Person
+        assertTrue(person is Person?)
+    }
+}
