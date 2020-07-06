84150acb3d39b580234bc93523cd9b18dda6695d
Ken Kousen <ken.kousen@kousenit.com>
2020-07-06 19:10:23 -0400

    Disable test for GitHub action

--- src/test/kotlin/io/JumbleTest.kt
+++ src/test/kotlin/io/JumbleTest.kt
@@ -1,9 +1,15 @@
 package io
 
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
+@Disabled("Dictionary not available on GitHub")
 class JumbleTest {
     private val jumble: Jumble = Jumble()
 
@@ -16,8 +22,15 @@ class JumbleTest {
         )
     }
 
+    @ParameterizedTest(name = "{0} unscrambles to {1}")
+    @CsvSource("cautla, actual",
+        "agileo, goalie", "mmlueb, mumble")
+    fun `unscramble words`(clue: String, answer: String) =
+        MatcherAssert.assertThat(jumble.solve(clue), Matchers.`is`(answer))
+
     @Test
     internal fun `check solveAll`() {
-        assertEquals(listOf("actual", "goalie", "mumble"), jumble.solveAll("cautla", "agileo", "mmlueb"))
+        assertEquals(listOf("actual", "goalie", "mumble"),
+            jumble.solveAll("cautla", "agileo", "mmlueb"))
     }
 }
