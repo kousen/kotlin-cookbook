f314d99a2ac65d14e545e2473d4413d07d7a94ca
Ken Kousen <ken.kousen@kousenit.com>
2023-02-02 16:13:11 -0500

    Reformat code

--- src/main/kotlin/datetime/groundhog.kt
+++ src/main/kotlin/datetime/groundhog.kt
@@ -6,7 +6,9 @@ fun main() {
     val groundHogDay = LocalDate(2023, Month.FEBRUARY, 2)
     val firstDaySpring = LocalDate(2023, Month.MARCH, 20)
     val days = groundHogDay.daysUntil(firstDaySpring)
+    val weeks = groundHogDay.until(
+        firstDaySpring, DateTimeUnit.WEEK)
+
     println("There are $days from GroundHog Day until the first day of Spring")
-    val weeks = groundHogDay.until(firstDaySpring, DateTimeUnit.WEEK)
     println("That is $weeks weeks and ${days - 7 * weeks} days")
 }
